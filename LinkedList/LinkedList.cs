2f382bf3c8f98ba16e4ab96001a414890cf2ab9b
Antoshyn4 <dmytro.antoshyn@nure.ua>
2021-04-01 12:48:08 +0300

    Создал поля для Списка

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList<T>
+    {
+        public Node<T> first { get; private set; }
+        public Node<T> last { get; private set; }
+
+        public int Length { get; private set; }
+    }
+}

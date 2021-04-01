bb2c14e5833c6d7496341520a4b2bc98b8e831a1
Antoshyn4 <dmytro.antoshyn@nure.ua>
2021-04-01 12:57:39 +0300

    Создал и перегрузил конуструктор Списка

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -8,13 +8,19 @@ namespace LinkedList
     {
         public Node<T> first { get; private set; }
         public Node<T> last { get; private set; }
-
         public int Length { get; private set; }
         public LinkedList()
         {
             first = null;
             last = null;
             Length = 0;
+        }
+        public LinkedList(T data)
+        {
+            var node = new Node<T>(data);
+            first = node;
+            last = node;
+            Length = 1;
         }
     }
 }
